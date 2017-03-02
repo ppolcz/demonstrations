718f60962ff438a87dbdcc11726139e053c58e19
Peter Polcz <ppolcz@gmail.com>
2017-03-02 21:43:15 +0100

    polcz.sty updated

--- Matlab/ccs/gyak3_sysrep_ctrb_obsb.m
+++ Matlab/ccs/gyak3_sysrep_ctrb_obsb.m
@@ -1,8 +1,8 @@
-%% 
-%  
+%%
+%
 %  file:   gyak3_sysrep_ctrb_obsb.m
-%  author: Polcz Péter <ppolcz@gmail.com> 
-%  
+%  author: Polcz Péter <ppolcz@gmail.com>
+%
 %  Created on 2017.03.02. Thursday, 14:14:49
 %
 
@@ -13,15 +13,15 @@ SCOPE_DEPTH = 0;
 TMP_QVgVGfoCXYiYXzPhvVPX = pcz_dispFunctionName;
 
 try c = evalin('caller','persist'); catch; c = []; end
-persist = pcz_persist(mfilename('fullpath'), c); clear c; 
+persist = pcz_persist(mfilename('fullpath'), c); clear c;
 persist.backup();
 %clear persist
 
 %% Example 1.
 % Give the state space representation of the following system
-% 
+%
 % $$ y^{(3)} + 6 \ddot y + 11 \dot y + 6 y = 2 \ddot u - 3 \dot u + 6 u $$
-% 
+%
 % Transfer function:
 b = [2 -3 6];
 a = [1 6 11 6];
@@ -40,9 +40,9 @@ D = sys.d;
 % (angular frequency scale)!
 figure, bode(H), grid on
 
-%% 
+%%
 % Plot the bode diagram MANUALLY!
-% 
+%
 % * First of all, we compute the transfer function $H(s)$
 syms s w
 Hs_sym = poly2sym(b,s) / poly2sym(a,s);
@@ -100,7 +100,7 @@ A = [-6 -4 ; 2 0];
 B = [4 ; 0];
 C = [0  1];
 
-%% 
+%%
 % Controllability matrix
 Cn = ctrb(A,B)
 Cn_by_hand = [B A*B]
@@ -120,10 +120,31 @@ On_by_hand = [C ; C*A]
 rank_On = rank(On)
 
 %%
-%  See also: 
+%  See also:
 %   - obsvf (Observability normal form)
 %   - ctrbf (Controllability normal form)
 
+%% Unobservable subspace
+% 
+% Starting the system from any initial condition within the
+% unobservability subspace, the output will be the same.
+
+A = [
+    -3  -2  0
+    1    0  0
+    1    0 -3
+    ];
+
+C = [ 1 0 0 ]
+On = obsv(A,C)
+unobsv = null(On)
+x0 = rand(3,1)
+
+figure, hold on
+for i = 1:10
+    [t,x] = ode45(@(t,x) A*x, [0, 10], x0 + unobsv*rand(size(unobsv,2),1));
+    plot(t,rand*0.02 +(C*x')')
+end
 %% end of the scope
 pcz_dispFunctionEnd(TMP_QVgVGfoCXYiYXzPhvVPX);
 clear TMP_QVgVGfoCXYiYXzPhvVPX
