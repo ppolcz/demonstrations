67d7e0850043afcd82c5c6a717a2d915aac93a3c
Peter Polcz <ppolcz@gmail.com>
2017-09-20 20:47:46 +0200

    trivial

---
+++ lib/matlab/pcs.m
@@ -0,0 +1,13 @@
+function pcs(varargin)
+%% Script pcs
+%  
+%  file:   pcs.m
+%  author: Peter Polcz <ppolcz@gmail.com> 
+%  
+%  Created on 2017. September 20.
+%
+%%
+
+pcz_createScript(varargin{:});
+
+end
