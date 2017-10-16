f14ae2b826b40bbe51aa2718dd7c30d0bdd3daca
Peter Polcz <ppolcz@gmail.com>
2017-10-17 00:22:03 +0200

    trivial

---
+++ oktatas/anal3/2_diffgeom/anal3_diffgeom_3_exterior_algebra.m
@@ -0,0 +1,28 @@
+%% Script anal3_diffgeom_3_exterior_algebra
+%  
+%  file:   anal3_diffgeom_3_exterior_algebra.m
+%  author: Peter Polcz <ppolcz@gmail.com> 
+%  
+%  Created on 2017. October 16.
+%
+%%
+
+% Automatically generated stuff
+global SCOPE_DEPTH
+SCOPE_DEPTH = 0;
+
+TMP_QVgVGfoCXYiYXzPhvVPX = pcz_dispFunctionName;
+
+try c = evalin('caller','persist'); catch; c = []; end
+persist = pcz_persist(mfilename('fullpath'), c); clear c; 
+persist.backup();
+%clear persist
+
+%%
+
+
+
+%%
+% End of the script.
+pcz_dispFunctionEnd(TMP_QVgVGfoCXYiYXzPhvVPX);
+clear TMP_QVgVGfoCXYiYXzPhvVPX
