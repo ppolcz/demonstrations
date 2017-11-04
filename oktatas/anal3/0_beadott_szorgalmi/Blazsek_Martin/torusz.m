24e199ab528eeaf5d470b828e70c08c4b3d28e8a
Peter Polcz <ppolcz@gmail.com>
2017-11-04 23:43:56 +0100

    Torus Martin

--- oktatas/anal3/0_beadott_szorgalmi/Blazsek_Martin/torusz.m
+++ oktatas/anal3/0_beadott_szorgalmi/Blazsek_Martin/torusz.m
@@ -1,10 +1,16 @@
-function torusz(R,r)
-    x = @(p,t) (R+r.*cos(p)).*cos(t);
-    y = @(p,t) (R+r.*cos(p)).*sin(t);
-    z = @(p,t) r.*sin(p);
-    fsurf(x,y,z, [-pi pi 0 2*pi]);
-    axis equal;
-    xlabel('x');
-    ylabel('y');
-    zlabel('z');
-end
+%% Tórusz
+%  Blazsek Martin János
+%  File: torus
+%%
+
+R = 5;
+r = 2;
+
+x = @(p,t) (R+r.*cos(p)).*cos(t);
+y = @(p,t) (R+r.*cos(p)).*sin(t);
+z = @(p,t) r.*sin(p);
+fsurf(x,y,z, [-pi pi 0 2*pi]);
+axis equal;
+xlabel('x');
+ylabel('y');
+zlabel('z');
