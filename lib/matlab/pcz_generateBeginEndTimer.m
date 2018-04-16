6cbb0dd05643fe6cb7843c9af0d00f25cc6806ee
Peter Polcz <ppolcz@gmail.com>
2018-04-16 16:12:05 +0200

    Iwasaki 2001 for DOA computation of Van der Pol

--- lib/matlab/pcz_generateBeginEndTimer.m
+++ lib/matlab/pcz_generateBeginEndTimer.m
@@ -1,18 +1,20 @@
 function varargout = pcz_generateBeginEndTimer
 %% 
 %  
-%  file:   pcz_generateBeginEndTimer.m
-%  author: Polcz P�ter <ppolcz@gmail.com> 
+%  File: pcz_generateBeginEndTimer.m
+%  Directory: 2_demonstrations/lib/matlab
+%  Author: Peter Polcz (ppolcz@gmail.com) 
 % 
 %  Created on 2016.01.17. Sunday, 13:30:51
+%  Modified on 2018. April 16.
 %
 
 %
 %%
 
 var = ['TMP_' pcz_generateString(20, 0) ];
-beginning = sprintf('%s = pcz_dispFunctionName;\n\n%%%%\n\n', var);
-ending = sprintf('\n%%%%\n%% End of the script.\npcz_dispFunctionEnd(%s);\nclear TMP_*\n\n', var);
+beginning = sprintf('%s = pcz_dispFunctionName;\n', var);
+ending = sprintf('\n%%%%\n\npcz_dispFunctionEnd(%s);\nclear TMP_*\n\n', var);
 
 %%
 if nargout == 1
