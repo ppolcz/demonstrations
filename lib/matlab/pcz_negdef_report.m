377007db6cec1d2082e940a734470e440dcb381f
Peter Polcz <ppolcz@gmail.com>
2018-04-26 18:00:53 +0200

    demonstrative - augment Pia

---
+++ lib/matlab/pcz_negdef_report.m
@@ -0,0 +1,80 @@
+function [r_eigs,r_extremum,r_perc] = pcz_negdef_report(A, prec, N, varargin)
+%% pcz_negdef_report
+%  
+%  File: pcz_negdef_report.m
+%  Directory: 2_demonstrations/lib/matlab
+%  Author: Peter Polcz (ppolcz@gmail.com) 
+%  
+%  Created on 2018. April 26.
+%
+
+%%
+
+if nargin < 3 || isempty(N) || ischar(N)
+    if nargin >= 3 && ischar(N)
+        varargin = [N varargin];
+    end
+    N = 'not used at the moment';
+end
+
+if nargin < 2 || isempty(prec) || ischar(prec)
+    if nargin >= 2 && ischar(prec)
+        varargin = [prec varargin];
+    end
+    prec = 10;
+    tol = 10^(-prec);
+elseif prec < 1
+    tol = prec;
+    prec = round(-log10(tol));
+else
+    tol = 10^(-prec);
+end    
+
+eigvals = eig(A);
+
+isdef = all([ real(eigvals) == eigvals ; real(eigvals) <= tol ]);
+
+indices = find(real(eigvals) > tol);
+
+perc = 1 - numel(indices)/numel(eigvals);
+
+extremum = max(eigvals);
+
+
+if nargout > 2
+    r_perc = perc;
+end
+
+if nargout > 1
+    r_extremum = extremum;
+end
+
+if nargout > 0
+    r_eigs = [ eigvals(indices,:) ; eigvals(setdiff(1:numel(eigvals),indices),:) ];
+end
+
+if nargout == 0    
+    if isdef
+        if ~isempty(varargin)
+            varargin{1} = [ varargin{1} 'Tolerance: %g,  worst eigenvalue: %g' ];
+        else
+            varargin{1} = 'Tolerance: %g,  worst eigenvalue: %g';
+        end
+        
+        varargin = [ varargin tol extremum ];
+    end
+    
+    pcz_info(isdef, varargin{:})
+    
+    if ~isdef
+        pcz_dispFunction('Worst eigenvalue: %g', extremum)
+        pcz_dispFunction('Percentage of good eigenvalues: %g%%', (1-perc)*100)
+        pcz_dispFunction('Tolerance: %g', tol)
+        pcz_dispFunction('Nr of bad eigenvalues/all: %d/%d', numel(indices), numel(eigvals))
+
+        % pcz_dispFunctionStackTrace
+        % pcz_dispFunctionNeedNewLine
+    end
+end
+
+end
---
+++ lib/matlab/pcz_posdef_report.m
@@ -0,0 +1,14 @@
+function [ret] = pcz_posdef_report
+%% pcz_posdef_report
+%  
+%  File: pcz_posdef_report.m
+%  Directory: 2_demonstrations/lib/matlab
+%  Author: Peter Polcz (ppolcz@gmail.com) 
+%  
+%  Created on 2018. April 26.
+%
+
+%%
+
+
+end
