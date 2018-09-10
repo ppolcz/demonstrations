47b4ef9335bb1a59e1a9e2c9f6134e1558b95730
Peter Polcz <ppolcz@gmail.com>
2018-09-10 15:26:35 +0200

    ebbol conflict lesz

---
+++ oktatas/anal3/segedszamitasok_2018_09_10_gyak1.m
@@ -0,0 +1,8 @@
+%% segedszamitasok_2018_09_10_gyak1
+%  
+%  File: segedszamitasok_2018_09_10_gyak1.m
+%  Directory: 2_demonstrations/oktatas/anal3
+%  Author: Peter Polcz (ppolcz@gmail.com) 
+%  
+%  Created on 2018. September 10.
+%
