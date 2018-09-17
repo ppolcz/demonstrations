b83e988172d8cd8c6a072a44f0e88706a631a849
Peter Polcz <ppolcz@gmail.com>
2018-09-17 23:28:08 +0200

    trivial

---
+++ lib/matlab/pcz_sdpvar2sym.m
@@ -0,0 +1,25 @@
+function [ret] = pcz_sdpvar2sym(name, var, assumption)
+%% pcz_sdpvar2sym
+%  
+%  File: pcz_sdpvar2sym.m
+%  Directory: 2_demonstrations/lib/matlab
+%  Author: Peter Polcz (ppolcz@gmail.com) 
+%  
+%  Created on 2018. September 17.
+%
+
+%%
+
+if nargin <= 2
+    assumption = 'real';
+end
+
+svar = sym(var);
+
+variables = symvar(svar);
+
+new_vars = sym(name, size(variables), assumption);
+
+ret = subs(svar, variables, new_vars);
+
+end
