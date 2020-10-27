2ec4f655c406c3aeabee9db4ca1cadf3f78f016b
Peter Polcz <ppolcz@gmail.com>
2020-10-27 16:02:41 +0100

    trivial

---
+++ workspace/konzultacio/Erdelyi_Aron_bilinear_progr/proba_1.m
@@ -0,0 +1,17 @@
+%%
+%  File: proba_1.m
+%  Directory: 2_demonstrations/workspace/konzultacio/Erdelyi_Aron_bilinear_progr
+%  Author: Peter Polcz (ppolcz@gmail.com) 
+%  
+%  Created on 2020. October 27. (2020b)
+%
+
+%%
+
+
+m = 5;
+
+rng(1);
+k = randn(m);
+c = randn(m);
+
