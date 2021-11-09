ee40f9738fc387f8e8589429b0bb7882760860b8
Peter Polcz <ppolcz@gmail.com>
2021-11-09 02:02:34 +0100

    Changes to be committed: 	new file:   lib/matlab/plotter_steps.m 	new file:   lib/matlab/plotter_steps_Log.m

---
+++ lib/matlab/plotter_steps.m
@@ -0,0 +1,12 @@
+function fig = plotter_steps(fname)
+%%
+%  File: plotter_steps.m
+%  Directory: 2_demonstrations/lib/matlab
+%  Author: Peter Polcz (ppolcz@gmail.com) 
+%  
+%  Created on 2021. November 04. (2021b)
+%
+
+
+
+end
---
+++ lib/matlab/plotter_steps_Log.m
@@ -0,0 +1,12 @@
+function fig = plotter_steps_Log(fname)
+%%
+%  File: plotter_steps_Log.m
+%  Directory: 2_demonstrations/lib/matlab
+%  Author: Peter Polcz (ppolcz@gmail.com) 
+%  
+%  Created on 2021. November 04. (2021b)
+%
+
+
+
+end
